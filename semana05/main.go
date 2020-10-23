20a9e7e162b25cbbbabc02130d272b74ac9b4220
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-10-22 22:01:25 -0500

    Struct Circulo definido y multiples maneras de inicializar la instancia.
    Se añadió un ejemplo de función normal que recibe como parametro
    un objeto por valor de la clase Circulo

---
+++ semana05/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circulo struct {
+	radio float64
+}
+
+func area(c Circulo) float64 {
+	return c.radio * c.radio * math.Pi
+}
+
+func main() {
+	c01 := Circulo{}
+	c02 := Circulo{radio: 10}
+	c03 := Circulo{15}
+	c04 := new(Circulo)
+	c05 := &Circulo{200}
+	fmt.Println(c01)
+	fmt.Println(c02)
+	fmt.Println(c03)
+	fmt.Println(c04)
+	fmt.Println(c05)
+}
