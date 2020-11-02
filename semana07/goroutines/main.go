ef8ee2b468256ccd2fb00319aa42147b8bd9c598
Ricardo Joel Gonzalez Velasco <70970994+ricardgo403@users.noreply.github.com>
2020-11-01 19:40:05 -0600

    Rename and add another main file

--- semana07/goroutines/main.go
+++ semana07/goroutines/main.go
@@ -9,12 +9,16 @@ func f(n int) {
 	count := 10
 	for i := 0; i < count; i++ {
 		fmt.Println(n, ": ", i)
-		time.Sleep(time.Second * 1)
+		time.Sleep(time.Millisecond * 100)
 	}
 }
 
 func main() {
 	go f(0)
+	go f(1)
+	go f(2)
+	go f(3)
+	go f(4)
 
 	var input string
 	fmt.Scanln(&input)
